9760b0f9a8c148fedcf2237bea935f8f1652deb4
sakkshm <saksham.saxena404@gmail.com>
2026-04-22 10:57:30 +0530

    sdk: removed sdk folder from tracking

--- sdk/python/bastion/generated_client/api/__init__.py
+++
@@ -1,9 +0,0 @@
-# flake8: noqa
-
-# import apis into api package
-from generated_client.api.files_api import FilesApi
-from generated_client.api.jobs_api import JobsApi
-from generated_client.api.sessions_api import SessionsApi
-from generated_client.api.system_api import SystemApi
-from generated_client.api.terminal_api import TerminalApi
-
